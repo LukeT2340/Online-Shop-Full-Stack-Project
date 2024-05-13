10ead74ac9dfcac8b102caa4bcb1b3910ef01d6b
LukeT2340 <lukethompson220@gmail.com>
2024-05-13 22:58:16 +1000

    Fixed server.js so it is ready to go live

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,6 @@ const cartController = require('./routers/cartController');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
-// Define port
-const port = 3002;
-
 // Define app
 const app = express();
 
@@ -29,7 +26,7 @@ app.use('/products', productController);
 app.use('/cart', cartController);
 
 // Start listening
-const server = app.listen(port, '0.0.0.0', () => {
-    const address = server.address();
-    console.log(`Server is listening at http://${address.address}:${address.port}`);
-  });
+const port = process.env.PORT || 3002;
+const server = app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
